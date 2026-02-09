45a51d063f353e85fa330c94ca015da8a7910b3b
friendstrell-svg <pravinthirukkumar@gmail.com>
2026-02-09 22:19:51 +0000

    Fix: Import SensitivityResult from correct module

--- components/TornadoChart.tsx
+++ components/TornadoChart.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine } from 'recharts'
-import { SensitivityResult } from '@/lib/model'
+import { SensitivityResult } from '@/lib/model/sensitivity'
 
 interface TornadoChartProps {
   data: SensitivityResult[]
